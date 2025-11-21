1a77e670b1f8f9b662604e6ff6cdadb9fa479ba2
Jacob Homanics <32080359+JacobHomanics@users.noreply.github.com>
2025-11-21 02:06:00 -0600

    Update ShieldPropertyDrawer.cs

--- Scripts/Editor/ShieldPropertyDrawer.cs
+++ Scripts/Editor/ShieldPropertyDrawer.cs
@@ -7,11 +7,6 @@ namespace JacobHomanics.HealthSystem.Editor
     [CustomPropertyDrawer(typeof(Shield))]
     public class ShieldPropertyDrawer : PropertyDrawer
     {
-        private static int dragControlID = -1;
-        private static float dragStartValue;
-        private static float dragStartMouseX;
-        private static string dragPropertyPath;
-
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             EditorGUI.BeginProperty(position, label, property);
@@ -24,64 +19,12 @@ namespace JacobHomanics.HealthSystem.Editor
             // Draw label
             EditorGUI.LabelField(labelRect, label);
 
-            // Draw value field with clamping and drag support
+            // Draw value field with clamping
             SerializedProperty valueProp = property.FindPropertyRelative("_value");
             if (valueProp != null)
             {
-                float currentValue = valueProp.floatValue;
-                int controlID = GUIUtility.GetControlID(FocusType.Passive);
-
-                // Create a draggable rect that includes both label and value
-                Rect dragRect = new Rect(valueRect.x - EditorGUIUtility.labelWidth, valueRect.y, EditorGUIUtility.labelWidth + valueRect.width, valueRect.height);
-
-                Event evt = Event.current;
-
-                string propertyPath = valueProp.propertyPath;
-
-                switch (evt.type)
-                {
-                    case EventType.MouseDown:
-                        if (dragRect.Contains(evt.mousePosition) && evt.button == 0)
-                        {
-                            dragControlID = controlID;
-                            dragStartValue = currentValue;
-                            dragStartMouseX = evt.mousePosition.x;
-                            dragPropertyPath = propertyPath;
-                            GUIUtility.hotControl = controlID;
-                            evt.Use();
-                        }
-                        break;
-
-                    case EventType.MouseDrag:
-                        if (GUIUtility.hotControl == controlID && dragControlID == controlID && dragPropertyPath == propertyPath)
-                        {
-                            float delta = (evt.mousePosition.x - dragStartMouseX) * 0.1f; // Sensitivity
-                            float newValue2 = dragStartValue + delta;
-                            valueProp.floatValue = Mathf.Max(0, newValue2);
-                            evt.Use();
-                        }
-                        break;
-
-                    case EventType.MouseUp:
-                        if (GUIUtility.hotControl == controlID && dragControlID == controlID && dragPropertyPath == propertyPath)
-                        {
-                            dragControlID = -1;
-                            dragPropertyPath = null;
-                            GUIUtility.hotControl = 0;
-                            evt.Use();
-                        }
-                        break;
-                }
-
-                // Show drag cursor when hovering over draggable area
-                if (dragRect.Contains(evt.mousePosition))
-                {
-                    EditorGUIUtility.AddCursorRect(dragRect, MouseCursor.SlideArrow);
-                }
-
-                // Draw the float field (still allows direct editing)
                 EditorGUI.BeginChangeCheck();
-                float newValue = EditorGUI.FloatField(valueRect, currentValue);
+                float newValue = EditorGUI.FloatField(valueRect, valueProp.floatValue);
                 if (EditorGUI.EndChangeCheck())
                 {
                     valueProp.floatValue = Mathf.Max(0, newValue);
