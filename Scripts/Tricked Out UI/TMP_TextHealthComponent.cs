469fec0dd352844b7c62b831614a230aa1ae403f
Jacob Homanics <32080359+JacobHomanics@users.noreply.github.com>
2025-11-21 00:42:12 -0600

    up

---
+++ Scripts/Tricked Out UI/TMP_TextHealthComponent.cs
@@ -0,0 +1,16 @@
+
+
+using UnityEngine;
+using JacobHomanics.TrickedOutUI;
+
+namespace JacobHomanics.HealthSystem.UI
+{
+    public class TMP_TextHealthComponent : TMP_TextVector2Component
+    {
+        public Health health;
+        // public override float X => health.Current;
+        // public override float Y => health.Max;
+
+
+    }
+}
